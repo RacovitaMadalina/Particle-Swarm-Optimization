fe812999a8d65e1a852f807783fb792ef9421def
Madalina Alina Racovita <Madalina.Racovita@endava.com>
2020-04-06 11:41:01 +0300

    Added functions to be minimized

---
+++ functions_to_be_minimized.py
@@ -0,0 +1,42 @@
+import math
+
+from constants import *
+
+
+def quadric_function(x):
+    return x[0] ** 2 - 2 * x[0] + 3
+
+
+def de_jong(x):
+    de_jong = 0
+    for i in range(len(x)):
+        de_jong += x[i] ** 2
+    return de_jong
+
+
+def six_hump_camel_back(x):
+    return (4 - 2.1 * (x[0] ** 2) + (x[0] ** 4) / 3) * x[0] ** 2 + x[0] * x[1] + (-4 + 4 * (x[1] ** 2)) * (x[1] ** 2)
+
+
+def rosenbrock(x):
+    rosenbrock_value = 0
+    for i in range(len(x) - 1):
+        rosenbrock_value = rosenbrock_value + 100 * ((x[i + 1] - x[i] ** 2) ** 2) + (1 - x[i]) ** 2
+    return rosenbrock_value
+
+
+def rastrigin(x):
+    rastrigin_value = 10 * DIMENSIONS_OF_THE_FUNCTION
+    for i in range(len(x)):
+        rastrigin_value = rastrigin_value + x[i] ** 2 - 10 * math.cos(2 * math.pi * x[i])
+    return rastrigin_value
+
+
+def griewangk(x):
+    sum = 0
+    for i in range(len(x)):
+        sum = sum + (x[i] ** 2) / 4000
+    product = 1
+    for i in range(len(x)):
+        product = product * math.cos(x[i] / math.sqrt(i + 1))
+    return sum + product + 1
