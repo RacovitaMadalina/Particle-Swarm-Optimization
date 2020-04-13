88059cf0ffe01cac4ceadd816c98a5501f85e950
Madalina Alina Racovita <Madalina.Racovita@endava.com>
2020-04-13 21:09:09 +0300

    Updated plots for time evolution of the minimum value

--- results_viz.py
+++ results_viz.py
@@ -1,22 +1,31 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 import plotly.express as px
 
-from constants import results_file
 from results import Results
 
-def best_value_depending_on_generation(df, title, xgap, filename = None):
-	for experiment_no in df["Experiment_no"].unique():
-		y = df[df["Experiment_no"] == experiment_no]["Mean"]
-		x = range(0, xgap * len(y), xgap)
-		plt.plot(x, y)
+matplotlib.style.use('ggplot')
 
-	print(len(df["Experiment_no"].unique()))
-	plt.title(title)
-	#plt.show()
 
-	if filename != None:
-		plt.savefig(filename)
+def best_value_depending_on_generation(df, title, xgap, filename=None):
+    fig = plt.figure(figsize=(18, 12))
+
+    for experiment_no in df["Experiment_no"].unique():
+        y = df[df["Experiment_no"] == experiment_no]["Mean"]
+        x = range(0, xgap * len(y), xgap)
+        plt.plot(x, y, lw=0.6)
+
+    print(len(df["Experiment_no"].unique()))
+
+    plt.title(title)
+    plt.ylabel('Minimum found')
+    plt.xlabel('Number of generations')
+    # plt.show()
+
+    if filename is not None:
+        plt.savefig(filename)
+
 
 rastrigin_res = Results()
 rastrigin_res.load("results/ratrigin.pickle")
@@ -31,32 +40,42 @@ griewangk_res = Results()
 griewangk_res.load("results/griewangk.pickle")
 
 pd.set_option("display.max_rows", len(rastrigin_res.df))
-#print(rastrigin_res.df)
-#exit(0)
+# print(rastrigin_res.df)
+# exit(0)
 
 # results.df.sort_values("Mean", inplace = True)
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 15) & (sixhump_res.df["Cognitive"] == 0.5)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 15.png")
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 50) & (sixhump_res.df["Cognitive"] == 0.5)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 50.png")
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 100) & (sixhump_res.df["Cognitive"] == 0.5)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 100.png")
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 15) & (sixhump_res.df["Cognitive"] == 1)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 15.png")
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 50) & (sixhump_res.df["Cognitive"] == 1)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 50.png")
-best_value_depending_on_generation(sixhump_res.df[(sixhump_res.df["Pop_size"] == 100) & (sixhump_res.df["Cognitive"] == 1)],
-	"Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 100.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 15) & (sixhump_res.df["Cognitive"] == 0.5)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 15.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 50) & (sixhump_res.df["Cognitive"] == 0.5)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 50.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 100) & (sixhump_res.df["Cognitive"] == 0.5)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 0.5, pop_size 100.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 15) & (sixhump_res.df["Cognitive"] == 1)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 15.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 50) & (sixhump_res.df["Cognitive"] == 1)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 50.png")
+best_value_depending_on_generation(
+    sixhump_res.df[(sixhump_res.df["Pop_size"] == 100) & (sixhump_res.df["Cognitive"] == 1)],
+    "Evolution of best values for Six Hump function", 1, "images/sh cognitive 1, pop_size 100.png")
 
 best_value_depending_on_generation(rosenbrock_res.df[rosenbrock_res.df["Cognitive"] == 0.5],
-	"Evolution of best values for Rosenbrock function", 10, "images/rose cognitive 0.5, pop_size 250.png")
+                                   "Evolution of best values for Rosenbrock function", 10,
+                                   "images/rose cognitive 0.5, pop_size 250.png")
 best_value_depending_on_generation(rosenbrock_res.df[rosenbrock_res.df["Cognitive"] == 1],
-	"Evolution of best values for Rosenbrock function", 10, "images/rose cognitive 1, pop_size 250.png")
+                                   "Evolution of best values for Rosenbrock function", 10,
+                                   "images/rose cognitive 1, pop_size 250.png")
 
 best_value_depending_on_generation(griewangk_res.df[griewangk_res.df["Cognitive"] == 0.5],
-	"Evolution of best values for Griewangk function", 10, "images/griew cognitive 0.5, pop_size 250.png")
+                                   "Evolution of best values for Griewangk function", 10,
+                                   "images/griew cognitive 0.5, pop_size 250.png")
 best_value_depending_on_generation(griewangk_res.df[griewangk_res.df["Cognitive"] == 1],
-	"Evolution of best values for Griewangk function", 10, "images/griew cognitive 1, pop_size 250.png")
+                                   "Evolution of best values for Griewangk function", 10,
+                                   "images/griew cognitive 1, pop_size 250.png")
 exit(0)
 
 fig = plt.figure(figsize=(15, 15))
@@ -68,5 +87,6 @@ fig = px.scatter_3d(results.df[results.df["Mean"] < 30],
                     color='Mean',
                     opacity=0.9,
                     size='Mean',
-                    title='The effect of different configurations for inertia / cognitive / social parameters over the PSO results.')
+                    title='The effect of different configurations for inertia /'
+                          ' cognitive / social parameters over the PSO results.')
 fig.show()
